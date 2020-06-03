dd4ade31f63d99e87d790a5f6d7020e5855389ac
fred.zhang <fred.zhang@zenlayer.com>
2020-06-03 21:11:01 +0800

    add feign,rest

--- user-consumer/src/main/java/com/fred/cloud/userconsumer/MainController.java
+++ user-consumer/src/main/java/com/fred/cloud/userconsumer/MainController.java
@@ -1,8 +1,12 @@
 package com.fred.cloud.userconsumer;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author fred
@@ -19,4 +23,30 @@ public class MainController {
     public String alive() {
         return userApi.alive();
     }
+
+    @GetMapping("/getById")
+    public String getById(/*@RequestParam */Integer id) {
+        System.out.println("id = " + id);
+        return userApi.getById(id);
+    }
+
+    @GetMapping("/getMap")
+    public Map getMap(@RequestParam Map<String,Object> map) {
+
+//        HashMap<String,Object> map1 = new HashMap<>(3);
+//        map.put("id",2000);
+//        map.put("name","fred");
+//        map.put("age", 20);
+
+        return userApi.getMap(map);
+    }
+
+    @GetMapping("/postUser")
+    User post(@RequestParam Map<String,Object> map) throws InvocationTargetException, IllegalAccessException {
+
+        User user = new User();
+        BeanUtils.copyProperties(user, map);
+        return userApi.post(user);
+    }
+
 }
--- user-consumer/src/main/java/com/fred/cloud/userconsumer/UserApi.java
+++ user-consumer/src/main/java/com/fred/cloud/userconsumer/UserApi.java
@@ -2,6 +2,11 @@ package com.fred.cloud.userconsumer;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 /**
  * @author fred
@@ -10,7 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
  *
  * name 可以写成 服务名。不用写 url
  */
-@FeignClient(name = "xx", url = "http://localhost:81")
+@FeignClient(name = "user-provider") // 直接 服务名，不用写 url
+//@FeignClient(name = "xx", url = "http://localhost:71")
 public interface UserApi {
 
     @GetMapping("/alive")
@@ -18,5 +24,32 @@ public interface UserApi {
 
     @GetMapping("/registry")
     public String registry();
+
+    /**
+     * 带参数，
+     *  所有的 web 注解都是给 feign 看的。需要 添加有所的相关的 注解RequestParam
+     *  组装 url 使用/
+     *
+     *
+     *
+     *  Resolved [org.springframework.web.HttpRequestMethodNotSupportedException: Request method 'POST' not supported]
+     *  需要添加 feign-httpclient
+     * @param id
+     * @return
+     */
+    @GetMapping("getById")
+    public String getById(@RequestParam("id") Integer id);
+
+    /**
+     * map
+     * @param map
+     * @return
+     */
+    @GetMapping("/getMap")
+    Map getMap(@RequestParam Map<String,Object> map);
+
+
+    @PostMapping("/postUser")
+    User post(@RequestBody User user);
 
 }
